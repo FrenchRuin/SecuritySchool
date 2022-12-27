dc6a9a9f9ca8e6f7ea264c64914ea002372127d5
이재찬 <toxic023@naver.com>
2022-12-27 21:31:49 +0900

    make School

---
+++ src/main/java/com/example/user/repository/SchoolRepository.java
@@ -0,0 +1,2 @@
+package com.example.user.repository;public interface SchoolRepository {
+}
---
+++ src/test/java/com/example/PaperUserTestApp.java
@@ -0,0 +1,13 @@
+package com.example.securityschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
