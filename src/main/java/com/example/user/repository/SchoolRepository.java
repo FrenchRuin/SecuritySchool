e690b96847a9ff078fb09355c49d15d767670d25
이재찬 <toxic023@naver.com>
2022-12-27 22:10:51 +0900

    domain add

--- src/main/java/com/example/user/repository/SchoolRepository.java
+++ src/main/java/com/example/user/repository/SchoolRepository.java
@@ -1,2 +1,15 @@
-package com.example.user.repository;public interface SchoolRepository {
+package com.example.user.repository;
+
+import com.example.user.domain.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
+    @Query("select distinct (city) from School")
+    List<String> getCities();
+
+    List<School> findAllByCity(String city);
 }
--- src/test/java/com/example/PaperUserTestApp.java
+++ src/test/java/com/example/PaperUserTestApp.java
@@ -1,13 +1,30 @@
-package com.example.securityschool;
+package com.example;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootTest
-class SecuritySchoolApplicationTests {
+@SpringBootApplication
+public class PaperUserTestApp {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(PaperUserTestApp.class, args);
     }
 
+
+    @Configuration
+    @ComponentScan("com.example.user")
+    @EnableJpaRepositories(basePackages = {
+            "com.example.user.repository"
+    })
+    @EntityScan(basePackages = {
+            "com.example.user.domain"
+    })
+    class Config{
+
+    }
 }
